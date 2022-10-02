fc74174d240f56c0a75bcc415e8f0d9d14894e4c
Gabriel-Alexandre-R-Pereira <profissionalgabriel96@gmail.com>
2022-10-01 22:44:03 -0300

    Banners Carousel scripts using JQuery

--- JS/script.js
+++ JS/script.js
@@ -17,6 +17,56 @@ function MobileMenu () {
 
 // Banners
 
+$(document).ready(function() {
+    let interval = window.setInterval(RotateBanners, 5000);
+
+    function RotateBanners() {
+        let $FirstBanner = $('#Carousel').find('figure:first');
+        let width = $FirstBanner.width();
+
+        $FirstBanner.animate({marginLeft: -width}, 1000, function() {
+            let $LastBanner = $('#Carousel').find('figure:last');
+            
+            $LastBanner.after($FirstBanner);
+            $FirstBanner.css({marginLeft: 0});
+        })
+    }
+
+    $('#LeftArrow').click(ToPreviousBanner);
+    $('#RightArrow').click(ToNextBanner);
+
+    function ToNextBanner() {
+        window.clearInterval(interval);
+
+        let $CurrentBanner = $('#Carousel').find('figure:first');
+        let width = $CurrentBanner.width();
+
+        $CurrentBanner.animate({marginLeft: -width}, 1000, function() {
+            let $LastBanner = $('#Carousel').find('figure:last');
+
+            $LastBanner.after($CurrentBanner);
+            $CurrentBanner.css({marginLeft: 0});
+
+            interval = window.setInterval(RotateBanners, 5000);
+        })
+    }
+
+    function ToPreviousBanner() {
+        window.clearInterval(interval);
+
+        let $CurrentBanner = $('#Carousel').find('figure:first');
+        let width = $CurrentBanner.width();
+        let $PreviousBanner = $('#Carousel').find('figure:last');
+
+        $PreviousBanner.css({marginLeft: -width});
+        $CurrentBanner.before($PreviousBanner);
+
+        $PreviousBanner.animate({marginLeft: 0}, 1000, function() {
+            interval = window.setInterval(RotateBanners, 5000);
+        })
+    }
+})
+
 
 
 
